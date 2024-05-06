f165352cc27b90d8087e8546b7b2c3af028ca821
AliKamar34 <aliekamar123@gmail.com>
2024-05-06 20:15:00 +0300

    create friend box action

--- lib/features/home/presentation/views/widgets/custom_friend_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_friend_app_bar.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:new_project/core/utils/colors_data.dart';
+import 'package:new_project/features/home/presentation/views/widgets/firend_box_action.dart';
 
 class CustomFriendsAppBar extends StatelessWidget {
   const CustomFriendsAppBar({
@@ -9,59 +9,23 @@ class CustomFriendsAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return const SizedBox(
       height: 65,
       width: double.infinity,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
+        padding: EdgeInsets.symmetric(horizontal: 15),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Text(
+            Text(
               'Friends',
               style: TextStyle(fontSize: 26),
             ),
-            Row(
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.transparent,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        bottomLeft: Radius.circular(20),
-                      ),
-                    ),
-                    padding: const EdgeInsets.all(8),
-                  ),
-                  child: const Text(
-                    'Search',
-                    style: TextStyle(color: ColorAssetData.kSecondaryColor),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.transparent,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(20),
-                        bottomRight: Radius.circular(20),
-                      ),
-                    ),
-                    padding: const EdgeInsets.all(8),
-                  ),
-                  child: const Text(
-                    'Requests',
-                    style: TextStyle(color: ColorAssetData.kSecondaryColor),
-                  ),
-                ),
-              ],
-            ),
+            FriendBoxAction(),
           ],
         ),
       ),
     );
   }
 }
+
---
+++ lib/features/home/presentation/views/widgets/firend_box_action.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:new_project/core/utils/colors_data.dart';
+
+class FriendBoxAction extends StatelessWidget {
+  const FriendBoxAction({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.transparent,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+              ),
+            ),
+            padding: const EdgeInsets.all(8),
+          ),
+          child: const Text(
+            'Search',
+            style: TextStyle(color: ColorAssetData.kSecondaryColor),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.transparent,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            padding: const EdgeInsets.all(8),
+          ),
+          child: const Text(
+            'Requests',
+            style: TextStyle(color: ColorAssetData.kSecondaryColor),
+          ),
+        ),
+      ],
+    );
+  }
+}
