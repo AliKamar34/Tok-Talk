49455a542302e2751a050033d75883cf46adcf99
AliKamar34 <aliekamar123@gmail.com>
2024-05-06 21:40:41 +0300

    add friends cubit in friends view

--- lib/features/home/presentation/views/friends_view.dart
+++ lib/features/home/presentation/views/friends_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:new_project/features/home/data/repos/home_repo_impl.dart';
+import 'package:new_project/features/home/presentation/manager/friends_cubit/friends_cubit.dart';
 import 'package:new_project/features/home/presentation/views/widgets/friends_view_body.dart';
 
 class FriendsView extends StatelessWidget {
@@ -6,8 +9,11 @@ class FriendsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: FriendsViewBody(),
+    return BlocProvider(
+      create: (context) => FriendsCubit(HomeRepoImpl())..getFriends(),
+      child: const Scaffold(
+        body: FriendsViewBody(),
+      ),
     );
   }
 }
--- lib/features/home/presentation/views/widgets/friend_info_list_view.dart
+++ lib/features/home/presentation/views/widgets/friend_info_list_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:new_project/features/home/data/models/person_model.dart';
+import 'package:new_project/features/home/presentation/manager/friends_cubit/friends_cubit.dart';
 import 'package:new_project/features/home/presentation/views/widgets/custom_friend_info_button.dart';
 
 class FriendInfoListView extends StatelessWidget {
@@ -7,17 +9,30 @@ class FriendInfoListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return CustomFriendInfoButton(
-          personModel: PersonModel(
-            email: 'ali@gmail.com',
-            name: 'ali',
-            image: 'https://en.m.wikipedia.org/wiki/File:Sample_User_Icon.png',
-          ),
-          onPressed: () {},
-        );
+    return BlocBuilder<FriendsCubit, FriendsState>(
+      builder: (context, state) {
+        if (state is FriendsSuccess) {
+          return ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return CustomFriendInfoButton(
+                personModel: PersonModel(
+                  email: 'ali@gmail.com',
+                  name: 'ali',
+                  image:
+                      'https://en.m.wikipedia.org/wiki/File:Sample_User_Icon.png',
+                ),
+                onPressed: () {},
+              );
+            },
+          );
+        } else if (state is FriendsFailuer) {
+          return Text(state.errMessage);
+        } else if (state is FriendsLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          return const Text('initial state');
+        }
       },
     );
   }
