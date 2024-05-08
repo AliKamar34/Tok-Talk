c993083b4067e3b6f287254e4dc41f7113c9cec0
AliKamar34 <aliekamar123@gmail.com>
2024-05-08 16:00:34 +0300

    create search result list view

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:new_project/features/search/presentation/views/widgets/custom_search_text_feild.dart';
+import 'package:new_project/features/search/presentation/views/widgets/search_result_list_view.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
@@ -7,8 +8,20 @@ class SearchViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomSearchTextFeild(),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          '   search Results ',
+          style: TextStyle(fontSize: 22),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Expanded(child: SearchResultListView()),
       ],
     );
   }
