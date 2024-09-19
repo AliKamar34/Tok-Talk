7991461fe11674c78bedc11e17aa4c254aacfc05
AliKamar34 <aliekamar123@gmail.com>
2024-09-20 01:32:02 +0300

    create profile view body

--- lib/features/settings/presentation/views/profile_view.dart
+++ lib/features/settings/presentation/views/profile_view.dart
@@ -1,65 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:new_project/core/models/person_model.dart';
-import 'package:new_project/features/home/presentation/views/widgets/custom_cirlce_image.dart';
-import 'package:new_project/features/settings/presentation/views/widgets/custom_profile_info_container.dart';
+import 'package:new_project/features/settings/presentation/views/widgets/profile_view_body.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key, required this.personModel});
   final PersonModel personModel;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ProfileViewBody(personModel: personModel),
-    );
-  }
-}
-
-class ProfileViewBody extends StatelessWidget {
-  const ProfileViewBody({super.key, required this.personModel});
-  final PersonModel personModel;
-  @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverFillRemaining(
-          hasScrollBody: false,
-          child: SizedBox(
-            height: MediaQuery.sizeOf(context).height,
-            child: Column(
-              children: [
-                Flexible(
-                  child: SizedBox(
-                    width: 250,
-                    child: CustomCircleImage(
-                      imageUrl: personModel.image,
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Column(
-                  children: [
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    CustomProfileInfoContainer(
-                      title: 'Name',
-                      subTitle: personModel.name,
-                      icon: Icons.account_circle,
-                    ),
-                    CustomProfileInfoContainer(
-                      title: 'Email',
-                      subTitle: personModel.email,
-                      icon: Icons.email,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
+    return SafeArea(
+      child: Scaffold(
+        body: ProfileViewBody(personModel: personModel),
+      ),
     );
   }
 }
--- lib/features/settings/presentation/views/widgets/profile_view_body.dart
+++ lib/features/settings/presentation/views/widgets/profile_view_body.dart
@@ -1,37 +1,53 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter/widgets.dart';
-// import 'package:new_project/features/home/presentation/views/widgets/custom_cirlce_image.dart';
-// import 'package:new_project/features/settings/presentation/views/widgets/custom_profile_info_container.dart';
 
-// class ProfileViewBody extends StatelessWidget {
-//   const ProfileViewBody({super.key});
+import 'package:flutter/material.dart';
+import 'package:new_project/core/models/person_model.dart';
+import 'package:new_project/features/home/presentation/views/widgets/custom_cirlce_image.dart';
+import 'package:new_project/features/settings/presentation/views/widgets/custom_profile_info_container.dart';
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         const Expanded(child: SizedBox()),
-//         Flexible(
-//           child: CustomCircleImage(
-//             imageUrl: FirebaseAuth.instance.currentUser!.photoURL ?? '',
-//           ),
-//         ),
-//         const SizedBox(
-//           height: 50,
-//         ),
-//         CustomProfileInfoContainer(
-//           onPressed: () {},
-//           title: 'Edit Profile Photo',
-//         ),
-//         CustomProfileInfoContainer(
-//           title: FirebaseAuth.instance.currentUser!.displayName!,
-//         ),
-//         CustomProfileInfoContainer(
-//           title: FirebaseAuth.instance.currentUser!.email!,
-//         ),
-//         const Expanded(flex: 2, child: SizedBox()),
-//       ],
-//     );
-//   }
-// }
+class ProfileViewBody extends StatelessWidget {
+  const ProfileViewBody({super.key, required this.personModel});
+  final PersonModel personModel;
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: SizedBox(
+            height: MediaQuery.sizeOf(context).height,
+            child: Column(
+              children: [
+                Flexible(
+                  child: SizedBox(
+                    width: 250,
+                    child: CustomCircleImage(
+                      imageUrl: personModel.image,
+                    ),
+                  ),
+                ),
+               
+                Column(
+                  children: [
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    CustomProfileInfoContainer(
+                      title: 'Name',
+                      subTitle: personModel.name,
+                      icon: Icons.account_circle,
+                    ),
+                    CustomProfileInfoContainer(
+                      title: 'Email',
+                      subTitle: personModel.email,
+                      icon: Icons.email,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
